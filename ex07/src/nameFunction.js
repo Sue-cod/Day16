7cb8ec117a4bdfc94d41578834d1e5bfc507fa57
Sue-cod <snozic892@gmail.com>
2021-03-24 02:58:46 +0100

    Funkcije

---
+++ ex07/src/nameFunction.js
@@ -0,0 +1,15 @@
+function firstName(firstName) {
+    return firstName.toUpperCase();
+}
+
+console.log("firstName".toUpperCase());
+exports.firstName = firstName;
+
+
+
+function lastName(lastName) {
+    return lastName.toUpperCase();
+}
+
+console.log("lastName".toUpperCase());
+exports.lastName = lastName;
